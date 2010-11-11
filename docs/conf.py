bdf28cbffbe88ebb3b891626b5019ad27298403c
Hong Minhee <minhee@dahlia.kr>
2010-11-11 20:21:16 +0900

    A trivial Sphinx config change.

--- docs/conf.py
+++ docs/conf.py
@@ -29,8 +29,7 @@ extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.doctest",
               "sphinx.ext.intersphinx",
               "sphinx.ext.todo",
-              "sphinx.ext.coverage",
-              "sphinx.ext.viewcode"]
+              "sphinx.ext.coverage"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
